dcb89caba2b9075bd13312b29e5a132475d35260
tonyandy5630 <tonyandy5630@gmail.com>
2022-05-19 00:45:37 +0700

    init

---
+++ FaceBookClone/scripts/scritp.js
@@ -0,0 +1,22 @@
+function showMore() {
+    var more = document.querySelectorAll("#more")
+    console.log("hi")
+    var moreButton = document.getElementById("more-button")
+    if (moreButton.style.display != "none") {
+        moreButton.style.display = "none"
+    } else {
+        moreButton.style.display = "flex"
+    }
+    for (let i = 0; i < more.length; i++) {
+        more[i].style.display = "flex"
+    }
+}
+
+function showLess() {
+    var more = document.querySelectorAll("#more")
+    var moreButton = document.getElementById("more-button")
+    moreButton.style.display = "flex"
+    for (let i = 0; i < more.length; i++) {
+        more[i].style.display = "none"
+    }
+}
