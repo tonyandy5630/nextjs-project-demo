0c0b966a8ba0a0c46ab4fa40343d5a7cc5ac0bfc
tonyandy5630 <tonyandy5630@gmail.com>
2022-05-26 16:51:41 +0700

    update on meeting container and main container

--- FaceBookClone/scripts/scritp.js
+++ FaceBookClone/scripts/scritp.js
@@ -1,19 +1,18 @@
 function showMore() {
-    var more = document.querySelectorAll("#more")
-    var moreButton = document.getElementById("more-button")
+    let mores = document.querySelectorAll("#more")
+    let moreButton = document.getElementById("more-button")
     if (moreButton.style.display != "none") {
         moreButton.style.display = "none"
     } else {
         moreButton.style.display = "flex"
     }
-    for (let i = 0; i < more.length; i++) {
-        more[i].style.display = "flex"
-    }
+
+    mores.forEach(more => more.style.display = 'flex')
 }
 
 function showLess() {
-    var more = document.querySelectorAll("#more")
-    var moreButton = document.getElementById("more-button")
+    let more = document.querySelectorAll("#more")
+    let moreButton = document.getElementById("more-button")
     moreButton.style.display = "flex"
     for (let i = 0; i < more.length; i++) {
         more[i].style.display = "none"
@@ -21,8 +20,8 @@ function showLess() {
 }
 
 function addOverlay(obj) {
-    var overlay = document.createElement('span');
-    var img = obj.querySelector("img")
+    let overlay = document.createElement('span');
+    let img = obj.querySelector("img")
     img.style.animation = "zoomIn 0.2s ease-out both"
     overlay.classList.add('story-overlay')
     obj.appendChild(overlay)
@@ -30,20 +29,54 @@ function addOverlay(obj) {
 
 function deleteOverlay(obj) {
     const overlay = document.querySelectorAll(".story-tray>.story-overlay")
-    var img = obj.querySelector("img")
+    let img = obj.querySelector("img")
     img.removeAttribute("animation")
     img.style.animation = "zoomOut 0.2s ease-in both"
-    for (var e of overlay) {
+    for (let e of overlay) {
         e.remove()
     }
 }
-var carousel = document.querySelector('.meet-carousel--container');
-var scrollWidth = carousel.offsetWidth;
+let carousel = document.querySelector('.meet-carousel--container');
+let carouselWidth = carousel.offsetWidth;
+let initialLoc = carousel.getBoundingClientRect().x;
+let leftBtn = document.querySelector('#left')
+let RightBtn = document.querySelector('#right')
+let parentWidth = document.querySelector('.feed-block--container').offsetWidth;
+let scrollLength = 0;
 
-function scrollLeft() {
-    carousel.style.transform = 'translateX(' + scrollWidth * 1 + 'px)';
+if (scrollLength <= 0) {
+    leftBtn.style.visibility = 'hidden'
+} else {
+    leftBtn.style.visibility = 'visible'
+}
+
+if (scrollLength > carouselWidth) {
+    RightBtn.style.visibility = 'hidden'
+} else {
+    leftBtn.style.visibility = 'visible'
+}
+
+
+function scrollToLeft() {
+    if (parentWidth < carouselWidth) {
+        if (scrollLength > 0) {
+            scrollLength -= parentWidth;
+            carousel.style.transform = 'translateX(' + scrollLength * -1 + 'px)';
+        }
+        console.log("scroll:" + scrollLength)
+    }
+
+
 }
 
 function scrollRight() {
-    carousel.style.transform = 'translateX(' + scrollWidth * -1 + 'px)';
+    if (parentWidth < carouselWidth) {
+        if (scrollLength <= carouselWidth) {
+            scrollLength += parentWidth;
+            carousel.style.transform = 'translateX(' + scrollLength * -1 + 'px)';
+        }
+
+        console.log("scroll left:" + scrollLength)
+    }
+
 }
